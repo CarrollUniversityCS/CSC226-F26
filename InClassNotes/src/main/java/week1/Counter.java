6bc2432e02e06621fdaa659907baf295924d5049
Jack Forden <60225273+JForden@users.noreply.github.com>
2026-09-03 23:37:52 +0000

    9/4 in class

---
+++ InClassNotes/src/main/java/week1/Counter.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Counter {
+
+    // 2. Add a private field
+    // TODO: Declare private int count;
+
+    // 3. Write the default constructor
+
+    // 4. Add an alternate constructor
+
+    // 5. Write an accessor method
+
+    // 6. Add an update method
+    
+    // 7. Overload increment
+   
+    // 8. Add a reset method
+
+    //9. Add a fixed-size history array that records the last 5 states of the counter. All logic must happen inside of the object and be hidden from the user.
+}
