43ae4c707dfc05f8243e9d4eb4d5e72f6b656bb9
Jack Forden <60225273+JForden@users.noreply.github.com>
2026-09-21 14:58:33 +0000

    added in class work 9-21

---
+++ InClassNotes/src/main/java/week4/LLNode.java
@@ -0,0 +1,22 @@
+package week4;
+
+public class LLNode<T> {
+    T info;
+    LLNode<T> next;
+
+    public LLNode(T info){
+        //constructor for our Node
+    }
+    public void setNext(){
+        //Setter for the node object
+    }
+    public LLNode<T> getNext(){
+       //get the next node in the chain
+    }
+    public void setInfo(){
+        //set the nodes info
+    }
+    public T getInfo(){
+        //get the nodes info
+    }
+}
